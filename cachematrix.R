487595b9bfedfa9d1890b3616f28c1a2bec5f188
pigcatdog1 <50623333+pigcatdog1@users.noreply.github.com>
2019-06-09 09:17:17 +0530

    Update cachematrix.R
    This is a function which uses the scoping rules of the R language

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+# #https-github.com-rdpeng-ProgrammingAssignment2
+##CacheMatrix
+## A set of 2 functions that will cache a matrix's inverse
 
-## Write a short comment describing this function
+##These functions creates a special object of class matrix which can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+    inve <- NULL
+    set <- function(y){
+      x <<- y
+      inve <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(solveinverse) inve <<- solveinverse
+    getInverse <- function() inve
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  }
 
-}
+## This function will  calculate the inverse of the matrix returned by makeCacheMatrix function.
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+    inve <- x$getInverse()
+    if(!is.null(inve)){
+      message("we will get cached data")
+      return(inve)
+    }
+    data <- x$get()
+    inve <- solve(data)
+    x$setInverse(inve)
+    inve      
+  }
+  
